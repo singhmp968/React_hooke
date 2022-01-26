c5a4318dfea1125e327ca84aba98745d7a3a5e0e
XinghMp968 <31666974+singhmp968@users.noreply.github.com>
2022-01-26 20:38:05 +0530

    useState of reactHook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+// we can use state in the functional based componenet using useState which is provided to us by react
+import React, { useState } from "react";
 function App() {
+  // here function useState is hook,useState is used to let you in 'state' in  react and this let you create local state
+  // useState will return  a array and wil be like this [currentState,function]  here currentState=> is current state and function is used to update the currentState
+  // HOOK: A hook is simply a function that let you hooks you into react features such as state or component lifecycle methods
+  // here serEmail will not merge with prev value but it will replace prev value
+  const [email, setEmail] = useState("a@a.com"); // this will accept only one argument and that argument will be initial state of our component
+  // const email = emailState[0]; // here @ 0th position we will get our currentState
+  // const setEmail = emailState[1]; // here we will get function and this will let us to upadte out=r state
+
+  /*  
+  const emailState = useState("a@a.com"); // this will accept only one argument and that argument will be initial state of our component
+  const email = emailState[0]; // here @ 0th position we will get our currentState
+  const setEmail = emailState[1]; // here we will get function and this will let us to upadte out=r state
+*/
+  const NameState = useState(""); // this will accept only one argument and that argument will be initial state of our component
+  const name = NameState[0]; // here @ 0th position we will get our currentState
+  const setName = NameState[1]; // here we will get function and this will let us to upadte out=r state
+
+  function handleChange(e) {
+    setEmail(e.target.value);
+  }
+  function handleChangeName(e) {
+    setName(e.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: 10 }}>
+      <input value={email} onChange={handleChange} />
+      <input value={name} onChange={handleChangeName} />
+
+      <p>Email: {email}</p>
+      <p>Name: {name}</p>
     </div>
   );
 }
